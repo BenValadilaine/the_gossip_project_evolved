2b045c5c52632b027d3fd7e57200a163bc9a971a
AmauryDBZ <amaury.desbazeille@gmail.com>
2020-08-03 20:08:55 +0200

    profile done

--- app/controllers/dynamic_pages_controller.rb
+++ app/controllers/dynamic_pages_controller.rb
@@ -1,11 +1,7 @@
 class DynamicPagesController < ApplicationController
 
   def profile
-    puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
-    puts name = params[:profile]
-    p @author = User.all.where(first_name: name)
-    p User.find(@author.id)
-
+    @author = User.find_by(first_name: params[:profile])
   end
 
 end

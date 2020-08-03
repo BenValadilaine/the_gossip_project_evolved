4085d596d73f0f0098438456e60325bc59b7f70a
BenValadilaine <benjamin.valadilaine@gmail.com>
2020-08-03 12:32:13 +0200

    À table

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+  def team
+    puts "Ceci est la page team"
+  end
+
+  def contact
+    puts "Ceci est la page contact"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_30_183123) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.integer "zip_code"
@@ -22,15 +25,15 @@ ActiveRecord::Schema.define(version: 2020_07_30_183123) do
   create_table "gossips", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_gossips_on_user_id"
   end
 
   create_table "join_table_gossip_tags", force: :cascade do |t|
-    t.integer "gossip_id"
-    t.integer "tag_id"
+    t.bigint "gossip_id"
+    t.bigint "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["gossip_id"], name: "index_join_table_gossip_tags_on_gossip_id"
@@ -39,8 +42,8 @@ ActiveRecord::Schema.define(version: 2020_07_30_183123) do
 
   create_table "private_messages", force: :cascade do |t|
     t.text "content"
-    t.integer "recipient_id"
-    t.integer "sender_id"
+    t.bigint "recipient_id"
+    t.bigint "sender_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["recipient_id"], name: "index_private_messages_on_recipient_id"
@@ -59,7 +62,7 @@ ActiveRecord::Schema.define(version: 2020_07_30_183123) do
     t.string "description"
     t.string "email"
     t.integer "age"
-    t.integer "city_id"
+    t.bigint "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["city_id"], name: "index_users_on_city_id"

6f3afe086379c256db6ca310fc7f83c049e4a10e
AmauryDBZ <amaury.desbazeille@gmail.com>
2020-08-03 21:04:01 +0200

    rails done

--- app/controllers/dynamic_pages_controller.rb
+++ app/controllers/dynamic_pages_controller.rb
@@ -1,7 +1,14 @@
 class DynamicPagesController < ApplicationController
 
   def profile
+    puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*******"
+    p params[:profile]
     @author = User.find_by(first_name: params[:profile])
   end
 
+  def gossip
+    puts "$$****************************************************"
+    puts params[:gossip]
+    @a_gossip = Gossip.find_by(title: params[:gossip])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   root 'landing_page#index'
   get '/welcome/:name_id', to: 'landing_page#welcome'
-  get '/:profile', to: 'dynamic_pages#profile', as: 'profile'
+  get '/gossip/:gossip', to: 'dynamic_pages#gossip', as: 'gossip'
+  get '/profile/:profile', to: 'dynamic_pages#profile', as: 'profile'
 end

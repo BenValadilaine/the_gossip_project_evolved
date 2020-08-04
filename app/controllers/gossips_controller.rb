8441100b1b2468206cacebdac31eecb883585bc6
BenValadilaine <benjamin.valadilaine@gmail.com>
2020-08-04 17:29:16 +0200

    Alles gut

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,19 +1,21 @@
 class GossipsController < ApplicationController
-    def show
-        @a_gossip = Gossip.find_by(title: params[:id])
+  def show
+    @a_gossip = Gossip.find_by(title: params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @gossip = Gossip.new(title: params[:gossip_title], content: params[:gossip_content], user: User.last)
+    if @gossip.save
+      @error = false
+      redirect_to :controller => "landing_page", :action => "index", :notice => "gossip_saved"
+    else
+      @error = true
+      render :action => "new"
     end
 
-    def new
-
-    end
-
-    def create
-        @gossip = Gossip.new(title: params[:gossip_title], content: params[:gossip_content], user: User.last)
-        if @gossip.save
-          redirect_to :controller => "landing_page", :action => "index"
-        else
-          render :action => "new"
-        end
-
-    end
+  end
 end

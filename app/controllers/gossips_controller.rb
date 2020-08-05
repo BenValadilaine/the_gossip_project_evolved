a02958e42a8f808dcbb10646cc4efea90a24997e
Juliet <juliet.dr@tutanota.com>
2020-08-05 13:04:02 +0200

    FIRST

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,6 +1,7 @@
 class GossipsController < ApplicationController
   def show
     @gossip = Gossip.find(params[:id])
+    @comment = Comment.new
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   root 'landing_page#index'
   get '/welcome/:name_id', to: 'landing_page#welcome'
-  resources :gossips
+  resources :gossips do
+    resources :comments
+  end
   resources :profiles, only: [:show]
   resources :cities, only: [:show]
 end
